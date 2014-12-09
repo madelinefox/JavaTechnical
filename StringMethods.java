b54fd78ceee98ca74f335863a0d5bac0ca160283
Aiden Fox <madelinemfox@gmail.com>
2014-12-08 19:39:03 -0500

    Create StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,34 @@
+public class StringMethods {
+	private String line;
+	
+	
+	/**
+	  *
+	  * @param line String to use and manipulate
+	public StringMethods(String line) {
+		this.line = line;
+	}
+	
+	public palendrome
+	
+	public longestPalendromeWithin
+	
+	public printRecurs
+	
+	public printInterative
+	
+	public removeChar
+	
+	public removeLocation
+	
+	public void print
+	
+	/**
+	  *
+	  * @return String representation of instance of StringMethods
+	  */
+	public String toString() {
+		return line;
+	}
+	
+	
